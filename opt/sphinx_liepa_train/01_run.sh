103520b9aa5d3dea0a5f55632632ec48334c5deb
Mondhs <mondhs@gmail.com>
2018-04-10 20:22:15 +0300

    if features are calcuated skip this step

--- opt/sphinx_liepa_train/01_run.sh
+++ opt/sphinx_liepa_train/01_run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 echo `date +"%T"`
 SECONDS=0
-sphinxtrain run
+
+if [ "$(ls -A ./feat)" ]; then
+    # skip feature calcuation. 
+    sphinxtrain -f verify run
+else
+    #Feature folder does not exists. act as normal
+    sphinxtrain run
+fi
+
+
 duration=$SECONDS
 echo `date +"%T"`
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
